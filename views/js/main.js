72ec8c4d479ade78213326649716c335b580496a
aviun <anastasiia.viun@gmail.com>
2016-02-10 16:02:56 -0500

    Counting of position correctlon moved outside the scope of for loop in updatePositions

--- views/js/main.js
+++ views/js/main.js
@@ -497,9 +497,15 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
+
+
+  var positionCorrection = [];
+  for (var j = 0; j < 5; j++) {
+    positionCorrection.push((Math.sin((document.body.scrollTop / 1250) + j)));
+  }
+
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
-    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+    items[i].style.left = items[i].basicLeft + 100 * positionCorrection[i % 5] + 'px';
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
